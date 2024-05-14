88b82b4b663f8fa7255302be8dedfbf137a364a0
TMTanmay1 <tanmaymeshram883@gmail.com>
2024-05-14 15:12:13 +0530

    image

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import { Container, Typography, TextField, Button, Grid, Box } from '@mui/material';
-import ContactImage from '../../assets/contact.jpg'; // Import your image
+import ContactImage from '../../assets/contact.png'; // Import your image
 
 function Contact() {
   return (
     <>
-    <Container sx={{display:'flex' , justifyContent:' center' , alignItems:'center'}}>
-      <Container style={{ marginTop: '50px', display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '20px' ,}}>
-        <Grid container justifyContent="center" spacing={4} style={{ marginBottom: '30px' }}>
-          
-          <Grid item xs={12} sm={6} md={4}>
-            <Box display="flex" justifyContent="center">
-              <img src={ContactImage} alt="Contact" style={{ width: 'auto', borderRadius: '5px', height:'400px'  }} />
+      <Container style={{ marginTop: '50px', display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '20px' }}>
+        <Grid container justifyContent="center" spacing={4}>
+          {/* Left Side - Image */}
+          <Grid item xs={12} sm={6}>
+            <Box display="flex" justifyContent="center" alignItems="center" style={{ height: '100%' }}>
+              <img src={ContactImage} alt="Contact" style={{ width: '100%', height: 'auto', maxHeight: '400px', borderRadius: '5px' }} />
             </Box>
           </Grid>
-          
-          <Grid item xs={12} sm={6} md={8}>
-            <div style={{ border: '2px solid #091a44', padding: '20px', borderRadius: '5px', maxWidth: '400px' }}>
+          {/* Right Side - Form */}
+          <Grid item xs={12} sm={6}>
+            <div style={{ border: '2px solid #091a44', padding: '20px', borderRadius: '5px' }}>
               <Typography variant="h4" style={{ color: '#091a44', marginBottom: '20px' }}>
                 Contact Customer Support
               </Typography>
@@ -47,7 +46,6 @@ function Contact() {
             </div>
           </Grid>
         </Grid>
-      </Container>
       </Container>
     </>
   );
